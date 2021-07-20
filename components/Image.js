1a71b3c997df46348567ce557ca65ba755fd0a24
nirus <nirus@live.in>
2021-07-21 02:23:43 +0530

    optimized the image

--- components/Image.js
+++ components/Image.js
@@ -1,5 +1,5 @@
 /*eslint-disable */
-import React, { useState } from 'react';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
 
 /**
  * Loads the Image asynchronously
@@ -7,19 +7,20 @@ import React, { useState } from 'react';
  * @returns 
  */
 export const ImageLoader = (props)=>{
-
-    const [loadedImage, setLoadedImage] = useState('https://via.placeholder.com/1200x600.png/6b6666/FFFFFF?text=Condenast%20-%20No%20image');
     
-    if(props.src){
-        const imageLoad = new Image();
-        imageLoad.src = props.src;
-        imageLoad.onload = function(){
-            // once memory cached in browser use the same link        
-            setLoadedImage(props.src);
-        };
-    }
+    const placeholder = 'https://via.placeholder.com/1200x600.png/6b6666/FFFFFF?text=Condenast%20-%20No%20image'
 
-    return <img alt={props.alt} src={loadedImage} loading="lazy" role="presentation" />
+    const [loadedImage, setLoadedImage] = useState(props.src || placeholder);
+    
+    return <img 
+        alt={props.alt} 
+        src={loadedImage} 
+        loading="lazy" 
+        role="presentation" 
+        onError={()=>{
+            setLoadedImage(placeholder);
+        }} 
+    />
 }
 
 export default ImageLoader;
--- components/NewsArticle.js
+++ components/NewsArticle.js
@@ -5,10 +5,11 @@ import ImageLoader from './Image';
  * Each new article link
  */
 export const News = (props)=>{
+    debugger;
     return  (
         <a target="_blank" rel="noreferrer" href={props.url} className={props.className}>
             <figure suppressHydrationWarning={true}>                
-                {process.browser  && <ImageLoader alt={props.title}src={props.image}></ImageLoader>}
+                {process.browser && <ImageLoader alt={props.title} src={props.image}></ImageLoader>}
                 <figcaption>
                     <h2>{props.title} &rarr;</h2>
                     <p>{props.description || 'NO DESCRIPTION AVAILABLE'}</p>
